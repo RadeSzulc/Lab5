e652e9810fafd351e830e54549f56dd79df88f0f
Radoslaw <r.szulc.762@studms.ug.edu.pl>
2021-05-01 18:31:53 +0200

    v1

--- scripts.js
+++ scripts.js
@@ -1,23 +1,4 @@
-const menu = document.querySelector('menu-context');
 
-
-function removeMenu() {
-    menu.style.left = '-9999px';
-    menu.style.top = '-9999px';
-}
-
-function rightButton(e) {
-    e.preventDefault();
-
-    if (e.button === 2) {
-        menu.style.left = e.pageX + 10 + 'px';
-        menu.style.top = e.pageY + 10 + 'px';
-    }
-}
-
-
-const block = document.querySelector('#right');
-block.addEventListener('mousedown', rightButton);
 
 
 
